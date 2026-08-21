a5baa0573c28e6968d5cb8cb3b927574c82e6bae
sabujAlom <sabujalom18@gmail.com>
2026-08-22 01:05:26 +0600

    added client verel link

--- src/config/auth.js
+++ src/config/auth.js
@@ -80,7 +80,7 @@ export const auth = betterAuth({
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://",
+    "https://resell-hub-client-three.vercel.app",
     process.env.CLIENT_URL
   ].filter(Boolean)
 });
