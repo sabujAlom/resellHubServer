2fba59cede3edcae27537fc4f9f32ee8a5b43159
sabujAlom <sabujalom18@gmail.com>
2026-08-22 17:06:10 +0600

    fix app.js file error

--- src/app.js
+++ src/app.js
@@ -208,7 +208,9 @@ app.get('/api/db-seed', async (req, res, next) => {
   }
 });
 
-
+app.get('/', (req, res) => {
+  res.send('ReSell Hub Server is running..');
+});
 
 // Fallback Error Handler
 app.use(errorHandler);
